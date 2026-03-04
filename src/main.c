6e154c66dd404b200c41951d7ac6d74f6607c2df
Nilesh Kumar Pahari <nileshpahari@protonmail.com>
2026-03-05 03:00:30 +0530

    refactor and add echo and type builtin

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +6,9 @@
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
+  const char *cmds[] = {"exit", "echo",
+                        "type"}; // array of pointer to str (not actual str arr)
+  size_t cmds_len = sizeof(cmds) / sizeof(cmds[0]);
   char *input = NULL;
   size_t len = 0;
 
@@ -14,7 +18,42 @@ int main(int argc, char *argv[]) {
       break;
     }
     input[strcspn(input, "\n")] = 0;
-    printf("%s: command not found\n", input);
+
+    if (input[0] == '\0') {
+      continue;
+    }
+
+    char *cmd = input;
+    char *rest = NULL;
+    char *space = strchr(input, ' ');
+
+    if (space) {
+      *space = 0;
+      rest = space + 1;
+    }
+
+    if (strcmp(input, "exit") == 0) {
+      break;
+    } else if (strcmp(cmd, "echo") == 0) {
+      printf("%s\n", rest ? rest : "");
+    } else if (strcmp(cmd, "type") == 0) {
+      bool flag = 0;
+      for (size_t i = 0; i < cmds_len; ++i) {
+        if (strcmp(cmds[i], rest) == 0) {
+          flag = 1;
+          break;
+        }
+      }
+
+      if (!flag) {
+        printf("%s: not found\n", rest);
+      } else {
+        printf("%s is a shell builtin\n", rest);
+      }
+
+    } else {
+      printf("%s: command not found\n", input);
+    }
   }
   free(input);
 
