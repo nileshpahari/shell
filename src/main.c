a902867a7cbbce8c90f1255e3efb221b1b4092a3
Nilesh Kumar Pahari <nileshpahari@protonmail.com>
2026-03-05 22:29:18 +0530

    add external program running support (unclean)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define PATH_LIST_SEPARATOR ":"
@@ -10,8 +11,7 @@ int main(int argc, char *argv[]) {
 
   char *path = getenv("PATH");
 
-  const char *cmds[] = {"exit", "echo",
-                        "type"}; // array of pointer to str (not actual str arr)
+  const char *cmds[] = {"exit", "echo", "type"};
   size_t cmds_len = sizeof(cmds) / sizeof(cmds[0]);
 
   char *input = NULL;
@@ -84,6 +84,55 @@ int main(int argc, char *argv[]) {
         free(full_path);
       } else {
         printf("%s: not found\n", rest);
+      }
+
+    } else if (path) {
+      int flag = 0;
+      char *copy_path = strdup(path);
+      char *dir = strtok(copy_path, PATH_LIST_SEPARATOR);
+
+      char *full_path = NULL;
+      while (dir) {
+        char full[1024];
+        snprintf(full, sizeof(full), "%s/%s", dir, cmd);
+
+        if (access(full, X_OK) == 0) {
+          pid_t pid = fork();
+          if (pid == 0) {
+            char **args = malloc(sizeof(char *));
+            size_t cnt = 1;
+            args[0] = cmd;
+
+            if (rest) {
+              char *arg = strtok(rest, " ");
+              while (arg) {
+                args = realloc(args, (cnt + 1) * sizeof(char *));
+                args[cnt++] = arg;
+                arg = strtok(NULL, " ");
+              }
+            }
+
+            args = realloc(args, (cnt + 1) * sizeof(char *));
+            args[cnt++] = NULL;
+
+            execv(full, args);
+            free(args);
+            perror("execv");
+            exit(1);
+          } else if (pid > 0) {
+            wait(NULL);
+          } else {
+            perror("fork");
+          }
+          flag = 1;
+          break;
+        }
+        dir = strtok(NULL, PATH_LIST_SEPARATOR);
+      }
+      free(copy_path);
+
+      if (!flag) {
+        printf("%s: command not found\n", input);
       }
 
     } else {
