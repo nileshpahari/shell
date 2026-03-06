c94519b56833e6b6b6970e063a00e217b1ff82cd
Nilesh Kumar Pahari <nileshpahari@protonmail.com>
2026-03-06 22:41:23 +0530

    add redirection

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +7,27 @@
 #include <unistd.h>
 
 #define PATH_LIST_SEPARATOR ":"
+
+char *trim(char *s) {
+  char *start = s;
+  char *end = s;
+
+  while (*start && isspace((unsigned char)*start))
+    start++;
+
+  while (*start) {
+    *end++ = *start++;
+  }
+  *end = '\0';
+
+  end--;
+  while (end >= s && isspace((unsigned char)*end)) {
+    *end = '\0';
+    end--;
+  }
+
+  return s;
+}
 
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
@@ -28,9 +51,46 @@ int main(int argc, char *argv[]) {
       continue;
     }
 
-    char *cmd = input;
+    char *input_copy2 = strdup(input);
+    char *out_redir = strstr(input_copy2, "1>");
+    int offset = 1;
+    if (!out_redir) {
+      out_redir = strchr(input_copy2, '>');
+      offset = 0;
+    }
+
+    char *cmd_part = input_copy2;
+    char *file_part = NULL;
+
+    if (out_redir) {
+      *out_redir = 0;
+      file_part = out_redir + 1 + offset;
+    }
+
+    trim(cmd_part);
+
+    int saved_stdout = -1;
+    int fd = -1;
+    if (file_part) {
+      trim(file_part);
+      fd = open(file_part, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+      if (fd < 0) {
+        perror("open");
+        continue;
+      }
+
+      saved_stdout = dup(STDOUT_FILENO);
+      dup2(fd, STDOUT_FILENO);
+      close(fd);
+    }
+
+    char *input_copy = strdup(input);
+    // char *cmd = input_copy;
+    // char *rest = NULL;
+    // char *space = strchr(input_copy, ' ');
+    char *cmd = cmd_part;
     char *rest = NULL;
-    char *space = strchr(input, ' ');
+    char *space = strchr(cmd_part, ' ');
 
     if (space) {
       *space = 0;
@@ -40,7 +100,25 @@ int main(int argc, char *argv[]) {
     if (strcmp(cmd, "exit") == 0) {
       break;
     } else if (strcmp(cmd, "echo") == 0) {
+      //     if (file_part) {
+      //       trim(file_part);
+      // trim(cmd_part);
+      //       char *cmd_args = strchr(cmd_part, ' ');
+      //       if (cmd_args)
+      //         cmd_args++;
+      //       else
+      //         cmd_args = "";
+      //       trim(cmd_args);
+      //       FILE *fp = fopen(file_part, "w");
+      //       if (fp == NULL) {
+      //         perror("fopen");
+      //   continue;
+      //       }
+      //       fprintf(fp, "%s\n", cmd_args);
+      //       fclose(fp);
+      //     } else {
       printf("%s\n", rest ? rest : "");
+      // }
     } else if (strcmp(cmd, "type") == 0) {
       if (!rest) {
         printf("type: missing argument\n");
@@ -91,6 +169,7 @@ int main(int argc, char *argv[]) {
       char *cwd = getcwd(NULL, 0);
       if (!cwd) {
         perror("getcwd");
+        continue;
       }
 
       printf("%s\n", cwd);
@@ -156,11 +235,18 @@ int main(int argc, char *argv[]) {
       free(copy_path);
 
       if (!flag) {
-        printf("%s: command not found\n", input);
+        printf("%s: command not found\n", input_copy);
       }
     } else {
-      printf("%s: command not found\n", input);
+      printf("%s: command not found\n", input_copy);
     }
+    if (file_part) {
+      fflush(stdout);
+      dup2(saved_stdout, STDOUT_FILENO);
+      close(saved_stdout);
+    }
+    free(input_copy);
+    free(input_copy2);
   }
   free(input);
 
